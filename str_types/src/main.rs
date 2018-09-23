5938ebb83e0f10fe4284793aeca65554e1156260
Johan Burell <johan.burell@gmail.com>
2018-09-23 18:58:07 +0200

    Added str mutability example

--- str_types/src/main.rs
+++ str_types/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let my_str = "Hello World!";
+    let mut my_str = "Hello World!";
+    my_str = my_str + " (&str)";
     println!("Value of str is: {}", my_str);
-    let my_string = my_str.to_string();
+    let mut my_string = my_str.to_string();
+    my_string = my_string + " (String)";
     println!("Value of string is: {}", my_string);
 }
