43b3b41bc4ed51f72f95ac85c3d5f45a7b9aece0
Johan Burell <johan.burell@gmail.com>
2018-09-23 16:46:58 +0200

    Added example with str and string

---
+++ str_types/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let my_str = "Hello World!";
+    println!("Value of str is: {}", my_str);
+    let my_string = my_str.to_string();
+    println!("Value of string is: {}", my_string);
+}
