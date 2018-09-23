575ebade50344a721ea49553b542fa55bf1cb604
Johan Burell <johan.burell@gmail.com>
2018-09-23 17:52:09 +0200

    Added example with numeric cast error

--- num_types/src/main.rs
+++ num_types/src/main.rs
@@ -1,17 +1,5 @@
-extern crate typename;
-use typename::TypeName;
-
 fn main() {
-    let a = 10;
-    let b = 5u32;
-    let c = 4.3;
-    let d = 5.1f32;
-    let e = 8usize;
-    println!("a({}): {}, b({}): {}, c({}): {}, d({}): {}, e({}): {}, e size: {}", 
-        a.type_name_of(), a, 
-        b.type_name_of(), b, 
-        c.type_name_of(), c, 
-        d.type_name_of(), d,
-        e.type_name_of(), e,
-        std::mem::size_of::<usize>());
+    let mut a = 5u32;
+    a = 4i32;
+    println!("a: {}", a);
 }
